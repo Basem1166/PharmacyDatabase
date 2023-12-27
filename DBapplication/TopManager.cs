d99e71b92fd0ef0b865e32e113d83ed0ebfe7b90
Basem1166 <33151311+Basem1166@users.noreply.github.com>
2023-12-27 19:32:14 +0300

    Terminate/add manager and add/delete branch in top manager

--- DBapplication/TopManager.cs
+++ DBapplication/TopManager.cs
@@ -24,5 +24,29 @@ namespace DBapplication
             SF.Show();
             this.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FireManager fm = new FireManager();
+            fm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AddManager f = new AddManager();
+            f.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AddBranch f = new AddBranch();
+            f.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DeleteBranch f = new DeleteBranch();
+            f.Show();
+        }
     }
 }
